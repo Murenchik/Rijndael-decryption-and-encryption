c236d2f37652eef00d09ba4e9658027a48af54c1
Denys Hyzhnyak <Denys.Hyzhnyak@livenau.net>
2020-07-06 15:00:55 +0300

    first commit

---
+++ Rijndael/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Rijndael
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the text: ");
+            string plainText = Console.ReadLine();
+
+            string passPhrase = "TestPass"; // Can be any string
+            string saltValue = "TestSalt"; // Can be any stribg
+            string hashAlgorithm = "SHA256"; // Can be "MD5" and "SHA256"
+            int passwordIterations = 2; // Can be any number
+            string initVector = "!2A4zadsw@3469AM"; // Must be 16 bytes
+            int keySize = 256;  // Can be 128 or 192
+
+            Console.WriteLine(String.Format("Not cipher test: {0}", plainText));
+
+            string cipherText = RijndaelAlg.Encrypt
+            (
+                plainText, passPhrase, saltValue, hashAlgorithm,
+                passwordIterations, initVector, keySize
+            );
+
+            Console.WriteLine(String.Format("Cipher text: {0}", cipherText));
+
+            plainText = RijndaelAlg.Decrypt
+            (
+                cipherText, passPhrase, saltValue, hashAlgorithm,
+                passwordIterations, initVector, keySize
+            );
+
+            Console.WriteLine(String.Format("Decryption text: {0}", plainText));
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Rijndael/RijndaelAlg.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+
+namespace Rijndael
+{
+    class RijndaelAlg
+    {
+
+
+        public static string Encrypt(string yourText, string password, string saltValue,
+            string hashAlgorithm, int passwordIteration, string initializationVector, int keySize)
+        {
+            byte[] initializationVectorBytes = Encoding.ASCII.GetBytes(initializationVector); // Transformation strings in byte massive
+            byte[] saltValueBytes = Encoding.ASCII.GetBytes(saltValue);
+
+            byte[] yourTextBytes = Encoding.UTF8.GetBytes(yourText); // Transformation open text to byte massive
+
+            PasswordDeriveBytes password1 = new PasswordDeriveBytes(password, saltValueBytes,
+                hashAlgorithm, passwordIteration); // Creation password with passPhrase and SaltValue, creation with hasg alg (SHA256 or MD5)
+
+            byte[] keyBytes = password1.GetBytes(keySize / 8); // Key size
+
+            RijndaelManaged symmetricKey = new RijndaelManaged(); // Create not initialize object of Rijndael cipher
+            symmetricKey.Mode = CipherMode.CBC;
+
+            ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, initializationVectorBytes); // Creation ecryptor from byte key and initVector
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+
+            cryptoStream.Write(yourTextBytes, 0, yourTextBytes.Length); // Start of cipher 
+
+            cryptoStream.FlushFinalBlock(); // Cipher last part 
+
+            byte[] cipherTextBytes = memoryStream.ToArray(); // Transformation encrypted data from MemoryStream to byte massive
+
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            string cipherText = Convert.ToBase64String(cipherTextBytes); // Transformation encrypted data to string (encoding base64)
+
+            return cipherText;
+        }
+
+        public static string Decrypt(string cipherText, string passPhrase, string saltValue, string hashAlgorithm,
+        int passwordIterations, string initVector, int keySize)
+        {
+            byte[] initializationVectorBytes = Encoding.ASCII.GetBytes(initVector); // Transformation strings in byte massive
+            byte[] saltValueBytes = Encoding.ASCII.GetBytes(saltValue);
+
+            byte[] cipherTextBytes = Convert.FromBase64String(cipherText); // Transformation open text to byte massive
+
+            PasswordDeriveBytes password1 = new PasswordDeriveBytes(passPhrase, saltValueBytes,
+                hashAlgorithm, passwordIterations); // Creation password with passPhrase and SaltValue, creation with hasg alg (SHA256 or MD5)
+
+            byte[] keyBytes = password1.GetBytes(keySize / 8); // Key size
+
+            RijndaelManaged symmetricKey = new RijndaelManaged(); // Create not initialize object of Rijndael cipher
+            symmetricKey.Mode = CipherMode.CBC;
+
+            ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, initializationVectorBytes); // Creation ecryptor from byte key and initVector
+
+            MemoryStream memoryStream = new MemoryStream(cipherTextBytes);
+
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read); // Spot Cryptographic stream
+
+            byte[] plainTextbytes = new byte[cipherTextBytes.Length];
+
+            int decryptedBytes = cryptoStream.Read(plainTextbytes, 0, plainTextbytes.Length); // Start of decryption
+
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            string plainText = Encoding.UTF8.GetString(plainTextbytes, 0, decryptedBytes); // Transformation encrypted data to string
+
+            return plainText;
+        }
+    }
+}
+
+
